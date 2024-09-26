0f35ed778f9d7bcedb93b0740459fc1f1804c51b
Suman Raj Sharma <sumanrajsharma@mac.lan>
2024-09-26 14:54:02 +1000

    chore: initial project setup

---
+++ quizora/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for quizora project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quizora.settings')
+
+application = get_asgi_application()
