a0cc0eb24f0c9a10d1f12f01ba2d787ea5a80bfe
Suman Raj Sharma <sumanrajsharma@Sumans-MacBook-Pro.local>
2024-09-27 17:00:50 +1000

    feat: Registered new viewset related to Leaderboard, QuizResult and ProgressTracking

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, QuestionViewSet, GameSessionViewSet
+from .views import (
+    UserViewSet, QuestionViewSet, QuestionOptionViewSet, GameSessionViewSet, QuizViewSet, 
+    QuizResultViewSet, ProgressTrackingViewSet, LeaderboardViewSet
+)
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
@@ -8,7 +11,12 @@ router = DefaultRouter()
 # Register the viewsets to specific endpoints
 router.register(r'users', UserViewSet)
 router.register(r'questions', QuestionViewSet)
+router.register(r'questionoptions', QuestionOptionViewSet)
+router.register(r'quizzes', QuizViewSet)
+router.register(r'quizresults', QuizResultViewSet)
 router.register(r'gamesessions', GameSessionViewSet)
+router.register(r'progresstracking', ProgressTrackingViewSet)
+router.register(r'leaderboard', LeaderboardViewSet)
 
 # The API URLs are now determined automatically by the router
 urlpatterns = [
